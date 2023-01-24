714165984c60388dbeb75ff7107dd19557a00c81
ImzyO <100032916+ImzyO@users.noreply.github.com>
2023-01-24 12:32:01 +0000

    pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Replicate code from the previous task.
+Implement a get_hyper method that takes the same arguments
+(and defaults) as get_page and returns a dictionary
+containing the following key-value pairs:
+"""
+import csv
+import math
+from typing import List
+
+
